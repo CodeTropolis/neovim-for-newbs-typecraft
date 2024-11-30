52d131842fc677dcff962f7da5725ec3af187f0e
Michael <michael@codetropolis.com>
2024-11-30 17:43:35 -0600

    Begin Mason LSP setup.

---
+++ lua/options.lua
@@ -0,0 +1,23 @@
+local o = vim.opt
+o.backup = false
+o.fileencoding = 'utf-8'
+o.relativenumber = true
+o.number = true
+o.autoindent = true
+o.expandtab = true
+o.tabstop = 2
+o.shiftwidth = 2
+o.scrolloff = 10
+o.hlsearch = true
+o.smartcase = true
+o.title = true
+o.backspace = 'indent,eol,start'
+o.showcmd = true
+o.wrap = false
+o.ai = true --Auto Indent
+o.si = true --Smart Indent
+o.path:append { '**' } --Go into subdirs on search.
+o.termguicolors = true
+o.clipboard = 'unnamedplus'
+
+
