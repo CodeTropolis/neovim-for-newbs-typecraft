d8f78be7ed6ea2d7fa7c87909ef041a973e23c45
Michael <michael@codetropolis.com>
2024-12-08 01:25:12 -0600

    Ep. 3 LSP.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,8 @@
-return { 
+return {
   "nvim-lualine/lualine.nvim",
   config = function()
-  require('lualine').setup({
-    options = {theme='dracula'}
-  })
+    require('lualine').setup({
+      options = {theme='dracula'}
+    })
   end
 }
